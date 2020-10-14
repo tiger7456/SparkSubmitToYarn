4475bc3ccc4409fb0691b29f990ca917f4be5c55
tiger7456 <tiger745612@qq.com>
2020-10-14 17:15:11 +0800

    测试

--- src/main/scala/com/scanflove/sparksql/LocalApp.scala
+++ src/main/scala/com/scanflove/sparksql/LocalApp.scala
@@ -6,11 +6,13 @@ import org.apache.spark.sql.SparkSession
 object LocalApp {
     def main(args: Array[String]): Unit = {
         //创建配置文件对象
-        val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkSQL01_MySQL")
+//        val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkSQL_LocalApp")
+        val conf: SparkConf = new SparkConf().setMaster("yarn-client").setAppName("SparkSQL_LocalApp")
         //创建SparkSession对象
         val spark: SparkSession = SparkSession.builder()
                 .config("mapred.input.dir.recursive", "true")
                 .config("hive.mapred.supports.subdirectories", "true")
+                .config("spark.driver.host", "192.168.0.107")
                 .enableHiveSupport().config(conf).getOrCreate()
 
         spark.sql("show databases").show()
